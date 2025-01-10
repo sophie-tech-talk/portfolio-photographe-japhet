4e596e9513189a99dfada09c7e7015c428983b3c
sophie <sogomispro@gmail.com>
2025-01-11 00:55:35 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<header class="main-header">
+    <div class="container">
+        <div class="logo">
+            <a href="index.php">
+                <img src="img/2.png">
+            </a>
+        </div>
+        <nav class="menu">
+            <ul>
+                <li><a href="index.php">Portfolio</a></li>
+                <li><a href="about.php">À propos</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="cta">
+            <a href="contact.php" class="btn-cta">Travaillons ensemble</a>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Photographie - Maks</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <div class="galerie">
+        <img src="img/aleksandr-popov-2GNBoMgKYEo-unsplash.jpg" />
+        <img src="img/evgeniya-pron-YEcwDXs61aU-unsplash.jpg" />
+        <img src="img/flavien-0xlWR3kJKiQ-unsplash.jpg" />
+        <img src="img/logan-weaver-lgnwvr-QjcuMh3rTBA-unsplash.jpg" />
+        <img src="img/michael-discenza-MxfcoxycH_Y-unsplash.jpg" />
+        <img src="img/mos-design-J4HEY-qZJTI-unsplash.jpg" />
+        <img src="img/moses-malik-roldan-eTvg2EMAOz4-unsplash.jpg" />
+        <img src="img/simon-spring-LA1JA3zjDdE-unsplash.jpg" />
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
